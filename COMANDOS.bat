289aad941ea56303fa0e52630673159711f255bd
Marcio Ferreira <hmarciogferreira@gmail.com>
2026-05-23 16:46:21 -0300

    Ajustes

--- COMANDOS.bat
+++ COMANDOS.bat
@@ -5,7 +5,7 @@ git config --global user.email "hmarciogferreira@gmail.com"
 git config --global user.name
 git config --global user.email
 
-
+// SUBIR PROJETO NO GITHUB
 echo "# site_fs60" >> README.md
 git init
 git add .
@@ -14,16 +14,17 @@ git branch -M main
 git remote add origin https://github.com/marciogferreira/site_fs60.git
 git push -u origin main
 -------------------------------------------------------------------------
+// SUBIR CODIGO DE ALTERACOES
 git add .
 git commit -m "Ajuste no Titulo do HTML"
 git push -u origin main
 -------------------------------------------------------------------------
+// COMANDO PARA BAIXAR ALTERACOES
+git pull
+-------------------------------------------------------------------------
 // REVERTER O GIT ADD .
 git restore --staged nome_arquivo.tipo
-
-
-
-
-CRIAR UMA PASTA E EXECUTAR O COMANDO DENTRO DELA
+-------------------------------------------------------------------------
+// CRIAR UMA PASTA E EXECUTAR O COMANDO DENTRO DELA
 git clone https://github.com/marciogferreira/site_fs60.git
 
